356d573dfc6b74c487de5be769ecd292acda61c7
Ayush Mehta <ayushmehtabps@gmail.com>
2025-11-13 23:09:05 +0530

    mongo-driver setup for golang

---
+++ server/magic-streamServer/models/movieModel.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Genre struct{
+	GenreID int
+	GenreName string
+}
+
+type Ranking struct{
+ RankingValue int
+ RankingName string
+}
+
+
+
+
+type Movie struct{
+	ID bson.ObjectID `"bson:_id" "json:_id"`
+	ImdbID string ``
+	Title string
+	PosterPath string
+	YoutubeID string
+	Genre []Genre
+	AdminReview string
+	Ranking Ranking
+
+}
