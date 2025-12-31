61a31c525f34efc4a62e5ca0da2d5487ad0714a5
Ayush Mehta <ayushmehtabps@gmail.com>
2025-12-31 18:22:42 +0530

    u

--- server/magic-streamServer/main.go
+++ server/magic-streamServer/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
-		"github.com/ayushmehta03/magic-stream/server/magic-streamServer/routes"
-		"go.mongodb.org/mongo-driver/v2/mongo"
-			database "github.com/ayushmehta03/magic-stream/server/magic-streamServer/database"
+	"log"
 
+	database "github.com/ayushmehta03/magic-stream/server/magic-streamServer/database"
+	"github.com/ayushmehta03/magic-stream/server/magic-streamServer/routes"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 
 	"github.com/gin-gonic/gin"
-
 )
 
 func main(){
@@ -18,12 +20,29 @@ func main(){
 	router:=gin.Default()
 
 
-
 	router.GET("/hello",func(c *gin.Context){
 		c.String(200,"hello, magic-stream-movies")
 	})
 
+	err:=godotenv.Load(".env")
+	if err!=nil{
+		log.Println("Warning: unable to find .env file")
+	}
+
 	 var client *mongo.Client=database.Connect()
+
+	if err:=client.Ping(context.Background(),nil);err!=nil{
+		log.Fatalf("Failed to reach server: %v",err)
+	}
+
+	defer func(){
+		err:=client.Disconnect(context.Background())
+		if err!=nil{
+			log.Fatalf("Failed to disconnect from MongoDB %v",err)
+		}
+	}()
+
+
 
 
 	routes.SetupUnProtectedRoutes(router,client)
