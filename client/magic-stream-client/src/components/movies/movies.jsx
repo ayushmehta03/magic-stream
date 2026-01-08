e41f2878cdfb4d70a3e671d18e53b7b1d46d953e
Ayush Mehta <ayushmehtabps@gmail.com>
2026-01-08 18:29:58 +0530

    add movie-component

---
+++ client/magic-stream-client/src/components/movies/movies.jsx
@@ -0,0 +1,13 @@
+import Movie from "../movie/Movie";
+
+const Movies=({movies,message})=>{
+
+    return(
+        <div className="container mt-4">
+
+            <div className="row"></div>
+
+        </div>
+    )
+
+}
